7cd351ab905f600f3c71bda83980ebe054eac05b
Savio Luan <savioluan16@gmail.com>
2023-11-30 19:09:32 -0300

    perf: :sparkles:

--- back-end/src/routes/index.router.ts
+++ back-end/src/routes/index.router.ts
@@ -1,6 +1,7 @@
 import { Router } from "express";
 import { userRoutes } from "./user.router";
+import { verifyEmail } from "../middlewares/verifyEmail.middleware";
 
 export const routes: Router = Router()
 
-routes.use('/users', userRoutes)
+routes.use('/users',verifyEmail, userRoutes)
