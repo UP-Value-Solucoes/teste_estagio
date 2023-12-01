86b6c091b218e7ab242cd1e2210089269e0f9877
Savio Luan <savioluan16@gmail.com>
2023-12-01 15:36:31 -0300

    fix: :zap: deploy nos scripts

--- back-end/src/database.ts
+++ back-end/src/database.ts
@@ -5,10 +5,25 @@ export const client: Client = new Client({
   password: process.env.DB_PASS,
   host: process.env.DB_HOST,
   database: process.env.DB_NAME,
-  port: Number(process.env.DB_PORT)
-})
+  port: Number(process.env.DB_PORT),
+});
 
 export const startDatabase = async (): Promise<void> => {
-  await client.connect()
-  console.log('Database connected')
-}
+  await client.connect();
+  console.log("Database connected");
+};
+export const createTables = async () => {
+  try {
+    const createTableQuery = `CREATE TABLE IF NOT EXISTS "users" (
+      "id" SERIAL PRIMARY KEY,
+      "name" VARCHAR(45) NOT NULL,
+      "email" VARCHAR(60) NOT NULL UNIQUE,
+      "password" VARCHAR(120) NOT NULL
+    );`;
+
+    await client.query(createTableQuery);
+    console.log("Tables are ready to work!");
+  } catch (error) {
+    console.log(error);
+  }
+};
--- back-end/src/server.ts
+++ back-end/src/server.ts
@@ -1,10 +1,11 @@
 import 'dotenv/config'
 import { app } from "./app";
-import { startDatabase } from "./database";
+import { createTables, startDatabase } from "./database";
 
 const PORT: number = Number(process.env.PORT) || 3000
 
 app.listen(PORT, async (): Promise<void> => {
   await startDatabase()
+  await createTables()
   console.log(`App running on port ${PORT}`)
 })
